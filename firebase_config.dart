860ffaf52b16bf4b1d3a3747ecc3193b3b907021
bpkoucme <bonprixkoucme@gmail.com>
2025-04-26 19:02:06 +0000

    Create firebase_config.dart

---
+++ firebase_config.dart
@@ -0,0 +1,9 @@
+// Fichier lib/firebase_config.dart
+const firebaseConfig = {
+  "apiKey": "AIzaSy80FN1uxZKtcVc_kstMe1mHoaFY9CNS3uA",
+  "authDomain": "monstockapp-73c82.firebaseapp.com",
+  "projectId": "monstockapp-73c82",
+  "storageBucket": "monstockapp-73c82.appspot.com",
+  "messagingSenderId": "903263639703",
+  "appId": "1:903263639703:web:ff8858aa21db9c25231d2"
+};
